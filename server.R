a133f657bac0c4fd40b81169dd2ebab943e82fba
Miranda Lin <mlin0119@uw.edu>
2017-11-30 18:47:14 -0800

    added ui and server files, close #5

---
+++ server.R
@@ -0,0 +1,30 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$map <- renderPlot({
+    
+  })
+  
+  output$plot1 <- renderPlot({
+    
+  })
+  
+  output$plot2 <- renderPlot({
+    
+  })
+  
+ 
+ 
+}
+)
---
+++ ui.R
@@ -0,0 +1,73 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Insert Title Here"),
+  navbarPage("",
+             
+             
+             
+    tabPanel("Map",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("plotType", "Plot type",
+            c("Scatter"="p", "Line"="l")
+          )
+        ),
+        mainPanel(
+          plotOutput("map")
+        )
+      )
+    ),
+             
+    
+    
+    tabPanel("Insert Name Here 1",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("plotType", "Plot type",
+            c("Scatter"="p", "Line"="l")
+          )
+        ),
+        mainPanel(
+          plotOutput("plot1")
+        )
+      )
+    ),
+    
+    
+    
+    
+    tabPanel("Insert Name Here 2",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("plotType", "Plot type",
+            c("Scatter"="p", "Line"="l")
+          )
+        ),
+        mainPanel(
+          plotOutput("plot2")
+        )
+      )
+    )
+
+    
+    
+  )
+))
+             
+    
+    
+  
+  
